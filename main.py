a3cf2f8a32548efc065e95bf58f388e86856153c
Salazar1019 <111543426+Salazar1019@users.noreply.github.com>
2023-05-25 19:09:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import scrapy
 
-class QuoteSpider( scrapy.Spider ):
-   name = 'quotes'
-   
-   def start_requests( self ):
-      urls = [
-         'https://quotes.toscrape.com/page/1/',
-         'https://quotes.toscrape.com/page/2/',
-      ]
-   
