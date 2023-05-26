e5f6938899c601745c469201b6878c3b14f90b6f
Salazar1019 <111543426+Salazar1019@users.noreply.github.com>
2023-05-25 19:07:33 -0500

    Update classExample.py

--- classExample.py
+++ classExample.py
@@ -1,11 +1,1 @@
 
-class Producto:
-  def _init_( self, nombre, precio, cantidad ):
-     self.nombre = nombre
-     self.precio = precio
-     self.cantidad = cantidad
-  def verProducto( self ):
-     print( "Producto: ", self.nombre, "\n   Precio: ", self.precio,  "\n   cantidad: " self.cantidad )
-
-kumis = Producto( "Kumis", 2000, 30 )
-kumis.verProducto()
